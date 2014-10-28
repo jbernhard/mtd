d52f2ddb6463d0e7a1a350ce5b4f9c019ddd9350
Jonah Bernhard <jonah.bernhard@gmail.com>
2014-10-27 20:28:12 -0400

    Implement priors.

--- mtd/priors.py
+++ mtd/priors.py
@@ -5,20 +5,47 @@ from __future__ import division
 import numpy as np
 from scipy import stats
 
-__all__ = 'FlatPrior', 'LogarithmicPrior', 'VariancePrior', 'LengthScalePrior'
+__all__ = 'FlatPrior', 'VariancePrior', 'LengthScalePrior', 'LogPrior'
 
 
-def FlatPrior():
-    pass
+def FlatPrior(lower=0., upper=1.):
+    """
+    Constant prior over a finite range.
+
+    """
+    return stats.uniform(loc=lower, scale=upper-lower)
 
 
-def LogarithmicPrior():
-    pass
+def VariancePrior(a=5., b=5.):
+    """
+    Inverse gamma prior for GP variance.
+
+    """
+    return stats.invgamma(a, scale=b)
 
 
-def VariancePrior():
-    pass
+def LengthScalePrior(a=1., b=0.1):
+    """
+    Beta prior for GP length scales (correlation lengths).
+
+    """
+    return stats.beta(a, b)
 
 
-def LengthScalePrior():
-    pass
+class _log_gen(stats.rv_continuous):
+    def _rvs(self, a):
+        return np.exp(np.random.uniform(np.log(a), 0, self._size))
+
+    def _pdf(self, x, a):
+        return 1/x  # not normalized!
+
+    def _logpdf(self, x, a):
+        return -np.log(x)  # not normalized!
+
+
+def LogPrior(lower=1e-8):
+    """
+    Logarithmic (Jeffreys) prior.
+
+    """
+    return _log_gen(a=1e-16, name='log')(lower)
--- mtd/test/test_priors.py
+++ mtd/test/test_priors.py
@@ -10,4 +10,40 @@ from .. import priors
 def test_priors():
     """prior distributions"""
 
-    pass
+    # This is a scipy.stats distribution so does not need much testing.
+    flat = priors.FlatPrior()
+    sample = flat.rvs(5)
+    assert np.all((sample >= 0) & (sample <= 1)), \
+        'FlatPrior:  must produce values in [0, 1].'
+    assert np.all(flat.logpdf(sample) == 0), \
+        'FlatPrior:  log PDF must be zero in [0, 1].'
+    assert np.all(np.isinf(flat.logpdf((-.5, 1.1)))), \
+        'FlatPrior:  log PDF must be -inf outside [0, 1].'
+
+    # This is a scipy.stats distribution so does not need much testing.
+    var = priors.VariancePrior()
+    sample = var.rvs(5)
+    assert np.all(sample > 0), \
+        'VariancePrior:  must produce values > 0.'
+    assert np.all(np.isinf(var.logpdf((-.5, -.01)))), \
+        'VariancePrior:  log PDF must be -inf for x < 0.'
+
+    # This is a scipy.stats distribution so does not need much testing.
+    length = priors.LengthScalePrior()
+    sample = length.rvs(5)
+    assert np.all((sample >= 0) & (sample <= 1)), \
+        'LengthScalePrior:  must produce values in [0, 1].'
+    assert np.all(np.isinf(length.logpdf((-.5, 1.1)))), \
+        'LengthScalePrior:  log PDF must be -inf outside [0, 1].'
+
+    # This one is implemented in mtd so it's tested a bit more rigorously.
+    log = priors.LogPrior()
+    sample = log.rvs(5)
+    assert np.all((sample >= 0) & (sample <= 1)), \
+        'LogPrior:  must produce values in [0, 1].'
+    assert np.all(log.logpdf(sample) == -np.log(sample)), \
+        'LogPrior:  log PDF must be -log(x).'
+    assert np.all(log.pdf(sample) == 1./sample), \
+        'LogPrior:  log PDF must be 1/x.'
+    assert np.all(np.isinf(log.logpdf((-.5, 0.)))), \
+        'LogPrior:  log PDF must be -inf for x <= 0.'
