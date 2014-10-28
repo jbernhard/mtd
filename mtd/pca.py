e917057530da3f1c431907703f891360de33149d
Jonah Bernhard <jonah.bernhard@gmail.com>
2014-10-28 13:47:16 -0400

    Implement PCA.

--- mtd/pca.py
+++ mtd/pca.py
@@ -3,14 +3,107 @@
 from __future__ import division
 
 import numpy as np
+from scipy import linalg
 
 __all__ = 'PCA',
 
 
+# adapted from sklearn.decomposition.PCA
 class PCA(object):
     """
     Principal component analysis.
 
+    y: (nsamples, nfeatures)
+        Array of observations.
+    npc: optional, integer or float in (0, 1)
+        If an integer, explicitly sets the number of PC.  If a float in (0, 1),
+        it sets the minimum explained variance fraction; the number of PC is
+        automatically determined.  If not provided, all PC are used.
+    normalize:  boolean, default false
+        Whether to normalize features to unit variance (i.e. divide the columns
+        of y by their standard deviation).
+
+    Note that the full SVD of y is always saved:
+      - PCA.weights and PCA.pc always return information for all PC.
+      - PCA.npc only determines the number of components used by transform()
+        and inverse(). It may be reset at any time.
+
     """
-    def __init__(self):
-        pass
+    def __init__(self, y, npc=None, normalize=False):
+        y = np.copy(np.atleast_2d(y))
+        self._mean = y.mean(axis=0)
+        y -= self._mean
+
+        if normalize:
+            self._std = y.std(axis=0)
+            y /= self._std
+        else:
+            self._std = None
+
+        self._svd = linalg.svd(y, full_matrices=False)
+
+        nfeatures = y.shape[1]
+        if npc is None or npc > nfeatures:
+            self.npc = nfeatures
+        elif 0 < npc < 1:
+            self.npc = np.count_nonzero(self.weights.cumsum() < npc) + 1
+        else:
+            self.npc = npc
+
+    @property
+    def weights(self):
+        """
+        Fraction of variance explained by each PC.
+
+        """
+        s_sq = np.square(self._svd[1])
+        return s_sq / s_sq.sum()
+
+    @property
+    def pc(self):
+        """
+        PC vectors.
+
+        """
+        return self._svd[2]
+
+    def transform(self, y=None):
+        """
+        Transform a set of observations into PC space.
+
+        y: (nobservations, nfeatures), optional
+            If not provided, the original y used to construct the class is
+            transformed.
+
+        """
+        # SVD:  y = U.S.Vt
+        # transformed y:  z = y.V = (U.S.Vt).V = U.S
+        # Therefore the left-singular vectors U can be reused to calculate the
+        # PC of the original y.  Right-singular vectors V are used for any
+        # other y.
+        if y is None:
+            U, s, Vt = self._svd
+            return (U[:, :self.npc]) * (s[:self.npc])
+
+        y = np.copy(np.atleast_2d(y))
+
+        y -= self._mean
+        if self._std is not None:
+            y /= self._std
+
+        return np.dot(y, self.pc[:self.npc].T)
+
+    def inverse(self, z):
+        """
+        Transform principal components back to feature space.
+
+        z: (nobservations, npc)
+
+        """
+        y = np.dot(z, self.pc[:self.npc])
+
+        if self._std is not None:
+            y *= self._std
+        y += self._mean
+
+        return y
--- mtd/test/test_pca.py
+++ mtd/test/test_pca.py
@@ -10,4 +10,107 @@ from .. import PCA
 def test_pca():
     """principal component analysis"""
 
-    pass
+    # trivial test data which can be fully described by one PC
+    nsamples, nfeatures = 5, 2
+    y = np.linspace(0, 1, nsamples*nfeatures).reshape(nsamples, nfeatures)
+
+    # test with 2 PC
+    pca = PCA(y)
+    assert np.allclose(pca.weights, (1, 0)), \
+        'Incorrect PC weights.\n{} != (1, 0)'.format(pca.weights)
+    assert np.allclose(np.abs(pca.pc), np.sqrt(2)/2), \
+        'Incorrect PCs.\n{} != +/-sqrt(2)/2'.format(pca.pc)
+    ratio = np.divide(*pca.pc)
+    assert np.allclose(ratio, (1, -1)) or np.allclose(ratio, (-1, 1)), \
+        'Incorrect PC ratio.\n{} != (+/-1, -/+1)'.format(ratio)
+
+    z1 = pca.transform()
+    z2 = pca.transform(y)
+    assert z1.shape == y.shape, \
+        'Transformed y has wrong shape.\n{} != {}'.format(z1.shape, y.shape)
+    assert np.allclose(z1, z2), \
+        'Transformations are inconsistent.\n{} != {}'.format(z1, z2)
+    y2 = pca.inverse(z1)
+    assert np.allclose(y2, y), \
+        ('Inverse transformation does not recover original data.\n'
+         '{} != {}'.format(y2, y))
+
+    # now check it still works with 1 PC
+    npc = 1
+    pca = PCA(y, npc=npc)
+    z = pca.transform()
+    assert z.shape == (nsamples, npc), \
+        'Transformed data should have {} PC, not {}.'.format(npc, z.shape[1])
+    y2 = pca.inverse(pca.transform(y))
+    assert np.allclose(y2, y), \
+        ('Inverse transformation does not recover original data.\n'
+         '{} != {}'.format(y2, y))
+
+    # random data
+    nsamples, nfeatures = 5, 3
+    y = np.random.rand(nsamples, nfeatures)
+    pca = PCA(y)
+
+    explained_var = pca.weights.sum()
+    assert np.allclose(explained_var, 1), \
+        'All PC must explain full variance.\n{} != 1'.format(explained_var)
+
+    z1 = pca.transform()
+    z2 = pca.transform(y)
+    assert z1.shape == y.shape, \
+        'Transformed y has wrong shape.\n{} != {}'.format(z1.shape, y.shape)
+    assert np.allclose(z1, z2), \
+        'Transformations are inconsistent.\n{} != {}'.format(z1, z2)
+    a = np.random.rand(2, nfeatures)
+    a2 = pca.inverse(pca.transform(a))
+    assert np.allclose(a2, a), \
+        ('Inverse transformation does not recover original data.\n'
+         '{} != {}'.format(a2, a))
+
+    mean = z1.mean(axis=0)
+    assert np.allclose(mean, 0), \
+        'Transformed y must have mean zero.\n{} != 0'.format(mean)
+
+    pc = pca.pc
+    inner = np.array([np.inner(pc[i], pc[j])
+                      for (i, j) in ((0, 1), (0, 2), (1, 2))])
+    assert np.allclose(inner, 0), \
+        'PC must be orthogonal.\n{} != 0'.format(inner)
+
+    # minimum explained variance
+    npc = nfeatures - 1
+    var = .99*pca.weights.cumsum()[npc - 1]
+    pca = PCA(y, npc=var)
+    print(pca.weights.cumsum())
+    assert pca.npc == 2, \
+        '{:g} of variance requires {} PC, not {}.'.format(var, npc, pca.npc)
+
+    z1 = pca.transform()
+    z2 = pca.transform(y)
+    shape = nsamples, npc
+    assert z1.shape == shape, \
+        'Transformed y has wrong shape.\n{} != {}'.format(z1.shape, shape)
+    assert np.allclose(z1, z2), \
+        'Transformations are inconsistent.\n{} != {}'.format(z1, z2)
+
+    # normalization
+    pca = PCA(y, normalize=True)
+    z1 = pca.transform()
+    z2 = pca.transform(y)
+    mean = z1.mean(axis=0)
+    assert np.allclose(mean, 0), \
+        'Transformed y must have mean zero.\n{} != 0'.format(mean)
+    var = z1.var()
+    assert np.allclose(var, 1), \
+        'Transformed y must have unit variance.\n{} != 1'.format(var)
+    assert np.allclose(z1, z2), \
+        'Transformations are inconsistent.\n{} != {}'.format(z1, z2)
+    y2 = pca.inverse(z1)
+    assert np.allclose(y2, y), \
+        ('Inverse transformation does not recover original data.\n'
+         '{} != {}'.format(y2, y))
+    a = np.random.rand(2, nfeatures)
+    a2 = pca.inverse(pca.transform(a))
+    assert np.allclose(a2, a), \
+        ('Inverse transformation does not recover original data.\n'
+         '{} != {}'.format(a2, a))
