5f881cd28e5a6b4a3641a4989015e5f45f0a849e
Jonah Bernhard <jonah.bernhard@gmail.com>
2014-11-04 19:19:36 -0500

    Clean up and simplify prior tests.

--- mtd/test/test_priors.py
+++ mtd/test/test_priors.py
@@ -12,56 +12,38 @@ from .. import priors
 def test_priors():
     """prior distributions"""
 
-    # This is a scipy.stats distribution so does not need much testing.
-    flat = priors.FlatPrior()
-    sample = flat.rvs(5)
-    assert np.all((sample >= 0) & (sample <= 1)), \
-        'FlatPrior:  must produce values in [0, 1].'
-    assert np.all(flat.logpdf(sample) == 0), \
-        'FlatPrior:  log PDF must be zero in [0, 1].'
-    assert np.all(np.isinf(flat.logpdf((-.5, 1.1)))), \
-        'FlatPrior:  log PDF must be -inf outside [0, 1].'
-    assert isinstance(pickle.loads(pickle.dumps(flat)), priors.Prior), \
-        'FlatPrior:  not pickleable.'
+    for name in priors.__all__:
+        if name == 'Prior':
+            continue
 
-    # This is a scipy.stats distribution so does not need much testing.
-    var = priors.VariancePrior()
-    sample = var.rvs(5)
-    assert np.all(sample > 0), \
-        'VariancePrior:  must produce values > 0.'
-    assert np.all(np.isfinite(var.logpdf(sample))), \
-        'VariancePrior:  sample log PDF is not finite.'
-    assert np.all(np.isinf(var.logpdf((-.5, -.01)))), \
-        'VariancePrior:  log PDF must be -inf for x < 0.'
+        p = getattr(priors, name)()
+        sample = p.rvs(10)
+        a, b = {
+            'FlatPrior': (0, 1),
+            'VariancePrior': (0, np.inf),
+            'LengthScalePrior': (0, 1),
+            'NoisePrior': (0, np.inf),
+        }[name]
 
-    # This is a scipy.stats distribution so does not need much testing.
-    length = priors.LengthScalePrior()
-    sample = length.rvs(5)
-    assert np.all((sample >= 0) & (sample <= 1)), \
-        'LengthScalePrior:  must produce values in [0, 1].'
-    assert np.all(np.isfinite(length.logpdf(sample))), \
-        'LengthScalePrior:  sample log PDF is not finite.'
-    assert np.all(np.isinf(length.logpdf((-.5, 1.1)))), \
-        'LengthScalePrior:  log PDF must be -inf outside [0, 1].'
-    assert isinstance(pickle.loads(pickle.dumps(length)), priors.Prior), \
-        'LengthScalePrior:  not pickleable.'
-
-    # This one is implemented in mtd so it's tested a bit more rigorously.
-    noise = priors.NoisePrior()
-    sample = noise.rvs(5)
-    assert np.all((sample >= 0) & (sample <= 1)), \
-        'NoisePrior:  must produce values in [0, 1].'
-    assert np.all(noise.logpdf(sample) == -np.log(sample)), \
-        'NoisePrior:  log PDF must be -log(x).'
-    assert np.all(noise[0].pdf(sample) == 1./sample), \
-        'NoisePrior:  log PDF must be 1/x.'
-    assert np.all(np.isinf(noise.logpdf((-.5, 0.)))), \
-        'NoisePrior:  log PDF must be -inf for x <= 0.'
-    assert isinstance(pickle.loads(pickle.dumps(noise)), priors.Prior), \
-        'NoisePrior:  not pickleable.'
+        assert np.all((sample >= a) & (sample <= b)), \
+            '{}: must produce values in [{}, {}].'.format(name, a, b)
+        assert np.all(np.isfinite(p.logpdf(sample))), \
+            '{}: sample log PDF is not finite.'.format(name)
+        assert np.all(np.isinf(p.logpdf((a - 1., b + 1.)))), \
+            '{}: log PDF must be -inf outside [{}, {}].'.format(name, a, b)
+        assert isinstance(pickle.loads(pickle.dumps(p)), priors.Prior), \
+            '{}: not pickleable.'.format(name)
 
     # Test combining priors.
-    prior = var + 2*length + noise
+    var = priors.VariancePrior()
+    length = priors.LengthScalePrior()
+    noise = priors.NoisePrior()
+
+    prior = length
+    prior *= 2
+    prior = var + prior
+    prior += noise
+
     assert len(prior) == 4, 'Combined prior has incorrect length.'
     assert all((
         prior[0] is var[0],
@@ -72,7 +54,10 @@ def test_priors():
 
     sample = prior.rvs(5)
     assert sample.shape == (5, 4), \
-        'Combined prior sample has incorrect shape.'
+        'Combined prior: sample has incorrect shape.'
+
     lp = tuple(prior.logpdf(s) for s in sample)
-    assert len(lp) == 5, 'Sample log PDF does not match sample size.'
-    assert np.all(np.isfinite(lp)), 'Sample log PDF is not finite.'
+    assert len(lp) == 5, \
+        'Combined prior: log PDF does not match sample size.'
+    assert np.all(np.isfinite(lp)), \
+        'Combined prior: sample log PDF is not finite.'
